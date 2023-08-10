397f83018fd8c05b1b1f4bce976ba155952e1c50
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2023-08-10 13:48:08 +0300

    Update comp/otelcol/bundle.go
    Co-authored-by: Pablo Baeyens <pablo.baeyens@datadoghq.com>

--- comp/otelcol/bundle.go
+++ comp/otelcol/bundle.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-// Package otelcol contains the OpenTelemetry Collector bundle pipeline to be included
+// Package otelcol contains the OTLP ingest bundle pipeline to be included
 // into the agent components.
 package otelcol
 
